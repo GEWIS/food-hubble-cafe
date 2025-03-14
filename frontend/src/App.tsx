dd5f28d0f804dd8b74e1adb3bb04dab75b0a4e67
Gijsdeman <github@deman.dev>
2025-03-14 12:36:23 +0100

    build: use js-configs presets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,69 @@
-import './index.css'
-import {useEffect, useState} from 'react';
-import {AnimatePresence, motion} from 'framer-motion';
+import './index.css';
+import { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Order {
-    number: number;
-    startTime: string;
-    timeoutSeconds: number;
+  number: number;
+  startTime: string;
+  timeoutSeconds: number;
 }
 
 function App() {
-    const [orders, setOrders] = useState<Order[]>([])
+  const [orders, setOrders] = useState<Order[]>([]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fetch('/api/getOrders')
-                .then(response => response.json())
-                .then(response => {
-                    setOrders(response.filter((order: Order) => {
-                        const timeTillOrderEnd = new Date(order.startTime).getTime() + order.timeoutSeconds * 1000 - Date.now();
-                        return timeTillOrderEnd > 0
-                    }))
-                })
-                .catch((e) => console.error(e))
-        }, import.meta.env.VITE_ORDER_INTERVAL || 5000);
-
-        return () => clearInterval(interval);
-    }, [])
-
-    return (
-        <div className="main">
-            <div className="header-container">
-                <img src="/hubble-logo.png" alt="Hubble Logo" className={"logo"}/>
-                <h1> Ready for pickup </h1>
-            </div>
-            <ul className="order-container">
-                <AnimatePresence>
-                    {
-                        orders.map((order) => (
-                            <motion.li className="order"
-                                       key={order.number}
-                                       initial={{scale: 0}}
-                                       animate={{
-                                           scale: 1,
-                                           transition: {delay: 0.5, type: "spring"}
-                                       }}
-                                       exit={{
-                                           opacity: 0,
-                                           transition: {delay: 0.5}
-                                       }}
-
-                                       layout
-
-                            >
-                                <div className="content">
-                                    <h1>{order.number}</h1>
-                                </div>
-                            </motion.li>
-                        ))
-                    }
-                </AnimatePresence>
-            </ul>
-        </div>
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        fetch('/api/getOrders')
+          .then((response) => response.json())
+          .then((response: Order[]) => {
+            setOrders(
+              response.filter((order) => {
+                const timeTillOrderEnd = new Date(order.startTime).getTime() + order.timeoutSeconds * 1000 - Date.now();
+                return timeTillOrderEnd > 0;
+              }),
+            );
+          })
+          .catch((e) => console.error(e));
+      },
+      (import.meta.env.VITE_ORDER_INTERVAL as number) || 5000,
     );
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="header-container">
+        <img src="/hubble-logo.png" alt="Hubble Logo" className={'logo'} />
+        <h1> Ready for pickup </h1>
+      </div>
+      <ul className="order-container">
+        <AnimatePresence>
+          {orders.map((order) => (
+            <motion.li
+              className="order"
+              key={order.number}
+              initial={{ scale: 0 }}
+              animate={{
+                scale: 1,
+                transition: { delay: 0.5, type: 'spring' },
+              }}
+              exit={{
+                opacity: 0,
+                transition: { delay: 0.5 },
+              }}
+              layout
+            >
+              <div className="content">
+                <h1>{order.number}</h1>
+              </div>
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './build'
+    outDir: './build',
   },
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:3001",
+      '/api': {
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
-  }
-})
+  },
+});
